07b51d40605414bc6027a9dd67cf2230d9be2ff6
Tushar Yerne <tusharyerne7d2@gmail.com>
2025-05-24 02:29:38 +0530

    Initial backend commit3

--- api/job_entries.js
+++ api/job_entries.js
@@ -1,49 +1,37 @@
+// /backend/api/job_entries.js
 import clientPromise from "../db.js";
+
 export default async function handler(req, res) {
   const client = await clientPromise;
-  const db = client.db(); // defaults to DB in your connection string
-  const collection = db.collection('jobs');
+  const db = client.db("jobAppDB");
+  const collection = db.collection("jobs");
 
-  if (req.method === 'GET') {
-    const jobs = await collection.find().toArray();
-    res.status(200).json(jobs);
-  }
+  switch (req.method) {
+    case "GET":
+      const jobs = await collection.find().toArray();
+      res.status(200).json(jobs);
+      break;
 
-  else if (req.method === 'POST') {
-    const body = req.body;
-    if (!body.name || !body.job || !body.jd) {
-      return res.status(400).json({ error: 'Missing fields' });
-    }
-    const result = await collection.insertOne(body);
-    res.status(200).json(result);
-  }
+    case "POST":
+      const newJob = req.body;
+      await collection.insertOne(newJob);
+      res.status(200).json({ message: "Job added" });
+      break;
 
-  else if (req.method === 'PUT') {
-    const id = req.query.id;
-    const body = req.body;
-    if (!id || !body.name || !body.job || !body.jd) {
-      return res.status(400).json({ error: 'Missing fields' });
-    }
+    case "PUT":
+      const { _id, ...updateFields } = req.body;
+      await collection.updateOne({ _id: new ObjectId(_id) }, { $set: updateFields });
+      res.status(200).json({ message: "Job updated" });
+      break;
 
-    const { ObjectId } = await import('mongodb');
-    const result = await collection.updateOne(
-      { _id: new ObjectId(id) },
-      { $set: { name: body.name, job: body.job, jd: body.jd } }
-    );
-    res.status(200).json(result);
-  }
+    case "DELETE":
+      const { id } = req.query;
+      await collection.deleteOne({ _id: new ObjectId(id) });
+      res.status(200).json({ message: "Job deleted" });
+      break;
 
-  else if (req.method === 'DELETE') {
-    const id = req.query.id;
-    if (!id) return res.status(400).json({ error: 'Missing ID' });
-
-    const { ObjectId } = await import('mongodb');
-    const result = await collection.deleteOne({ _id: new ObjectId(id) });
-    res.status(200).json(result);
-  }
-
-  else {
-    res.setHeader('Allow', ['GET', 'POST', 'PUT', 'DELETE']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    default:
+      res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
